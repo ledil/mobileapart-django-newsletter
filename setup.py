f0219107fe9386e75201d72bbcfae839c3c194d9
Leonardo Di Lella <leonardo.dilella@mobileapart.com>
2012-07-04 18:04:43 +0200

    fixed typo

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from mobileapart.django import newsletter
 
 
 setup(name='mobileapart.django.newsletter',
-      version=newsletter.__version__,
+      version="0.4",
       description='A Django app for sending newsletter by email to a contact list.',
       long_description='A Django app for sending newsletter by email to a contact list.',
       keywords='django, newsletter, mailing',
